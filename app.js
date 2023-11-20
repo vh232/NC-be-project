c3ca861a673b73684b668ffcaf17d39083658aab
Vicky <127982655+vh232@users.noreply.github.com>
2023-11-20 16:08:36 +0000

    add feedback

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
-const { getAllTopics, getEndpoints, getAllArticles } = require('./controllers/controllers');
+const { getAllTopics, getArticleById, getEndpoints, getAllArticles } = require('./controllers/controllers');
 const { handleServErr, handleCustomErr, handlePsqlErr } = require('./errorhandling');
 const app = express();
 
 app.get('/api/topics', getAllTopics);
 
 app.get('/api', getEndpoints);
+
+app.get('/api/articles/:article_id', getArticleById);
 
 app.get('/api/articles', getAllArticles)
 
--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,5 +1,4 @@
-const { getEachTopic, printEndpoints, getEachArticle } = require("../models/models")
-
+const { getEachTopic, getSingleArticle, printEndpoints, getEachArticle } = require("../models/models")
 
 exports.getAllTopics = (req, res, next) => {
     getEachTopic()
@@ -13,6 +12,17 @@ exports.getEndpoints = (req, res, next) => {
     const endpoints = printEndpoints()
     return res.status(200).send({ endpoints }
     )}
+
+exports.getArticleById = (req, res, next) => {
+    const id = req.params.article_id
+    getSingleArticle(id)
+    .then((article) => {
+        res.status(200).send({ article })
+    })
+    .catch(next);
+}
+    
+
 
 exports.getAllArticles = (req, res, next) => {
     getEachArticle()
