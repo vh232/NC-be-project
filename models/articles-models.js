71e59ee87e65de155a2e4768c3ad5609de87e56b
Vicky <127982655+vh232@users.noreply.github.com>
2023-11-21 17:05:24 +0000

    removed duplicate after merge, deleted original cont/model files

--- models/articles-models.js
+++ models/articles-models.js
@@ -1,19 +1,5 @@
 const db = require("../db/connection");
 
-exports.getEachArticle = () => {
-    let queryStr = `SELECT articles.*, COUNT(articles.article_id) AS comment_count FROM articles LEFT JOIN comments ON articles.article_id = comments.article_id GROUP BY articles.article_id `;
-  
-    queryStr += "ORDER BY created_at ASC";
-    return db.query(queryStr).then(({ rows }) => {
-      const rowsCopy = JSON.parse(JSON.stringify(rows));
-      const noBodyRows = rowsCopy.map((row) => {
-        delete row.body;
-        return row;
-      });
-      return noBodyRows;
-    });
-  };
-
   exports.patchSingleArticle = (inputId, voteUpdate) => {
     const queryVals = [inputId, voteUpdate]
     let queryStr = `UPDATE articles SET votes = votes + $2 WHERE article_id = $1 RETURNING *;`
